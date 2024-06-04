80a349bbae17894e515989babe870e8a30582d7b
wliu40 <wliu0828@gmail.com>
2024-06-03 20:54:23 -0400

    parents to child communication

---
+++ components-test/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import Friend from './components/Friend.vue';
+</script>
+
+<template>
+  <div id="App">
+    <Friend 
+    v-for='friend in friends'
+      :key="friend.id"
+      :friendName="friend.name"
+      :phoneNumber="friend.phone"
+      :emailAddress="friend.email">  </Friend>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    Friend,
+  },
+  data(){
+    return{
+      friends: [
+          {
+            id: "manuel",
+            name: "Manuel Lorenz",
+            phone: "0123 45678 90",
+            email: "manual@yahoo.com",
+          },
+          {
+            id: "julie",
+            name: "Julie Jones",
+            phone: "0987 654421 21",
+            email: "julie@yahoo.com",
+          }
+        ]
+      }
+    }
+};
+</script>
---
+++ components-test/src/components/Friend.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="Friend">
+        <strong>{{ friendName }}</strong>
+        <button @click="toggleDetails">{{ detailsAreVisible? 'Hide': 'Show' }}</button>
+        <ul v-if="detailsAreVisible">
+
+            <p>Phone: {{ phoneNumber }}</p>
+            <p>Email: {{ emailAddress }}</p>
+
+
+        </ul>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        detailsAreVisible: false,
+      };
+    },
+    props: {
+        friendName: {
+            type: String,
+            required: true
+        },
+        emailAddress: {
+            type: String,
+            required: true
+        },  
+        phoneNumber: {
+            type: String,
+            required: true,
+            default: '012 345 6789'
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+  };
+</script>
