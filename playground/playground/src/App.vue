27960b0d795dd47eb6296faf4f015a65f7789620
wliu40 <wliu0828@gmail.com>
2024-06-05 19:13:51 -0400

    comments experiments

---
+++ playground/playground/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Comment
+      v-for="(comment, index) in comments"
+      :key="index"
+      :comment="comment"
+      @rename="renameComment(index, $event)"
+      @delete="deleteComment(index)"
+    />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import Comment from './components/Comments.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Comment
+  },
+  setup() {
+    const comments = ref([
+      'This is the first comment.',
+      'This is the second comment.',
+      'This is the third comment.'
+    ]);
+
+    const renameComment = (index, newComment) => {
+      comments.value[index] = newComment;
+    };
+
+    const deleteComment = (index) => {
+      comments.value.splice(index, 1);
+    };
+
+    return {
+      comments,
+      renameComment,
+      deleteComment
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
+</style>
---
+++ playground/playground/src/components/Comment.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="comment-container">
+      <div v-if="isEditing">
+        <input
+          v-model="editableComment"
+          @keyup.enter="saveComment"
+          @blur="saveComment"
+          class="comment-input"
+          ref="input"
+        />
+      </div>
+      <p v-else class="comment-text">{{ comment }}</p>
+      <div class="menu-container">
+        <span class="three-dots" @click="toggleMenu">...</span>
+        <div class="dropdown-menu" v-if="showMenu">
+          <button class="menu-option" @click="editComment">Rename</button>
+          <button class="menu-option" @click="deleteComment">Delete</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Comment',
+    props: {
+      comment: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        showMenu: false,
+        isEditing: false,
+        editableComment: this.comment
+      };
+    },
+    methods: {
+      toggleMenu() {
+        this.showMenu = !this.showMenu;
+      },
+      handleClickOutside(event) {
+        if (!this.$el.contains(event.target)) {
+          this.showMenu = false;
+        }
+      },
+      editComment() {
+        this.isEditing = true;
+        this.showMenu = false;
+        this.$nextTick(() => {
+          this.$refs.input.focus();
+        });
+      },
+      saveComment() {
+        this.isEditing = false;
+        this.$emit('rename', this.editableComment);
+      },
+      deleteComment() {
+        if (confirm("Are you sure you want to delete this comment?")) {
+          this.showMenu = false;
+          this.$emit('delete');
+        }
+      }
+    },
+    watch: {
+      comment(newVal) {
+        this.editableComment = newVal;
+      }
+    },
+    mounted() {
+      document.addEventListener('click', this.handleClickOutside);
+    },
+    beforeDestroy() {
+      document.removeEventListener('click', this.handleClickOutside);
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .comment-container {
+    position: relative;
+    border: 1px solid #ccc;
+    padding: 10px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 400px;
+    margin: 20px;
+  }
+  
+  .comment-text {
+    margin: 0;
+  }
+  
+  .comment-input {
+    width: 100%;
+    padding: 5px;
+    box-sizing: border-box;
+  }
+  
+  .menu-container {
+    position: relative;
+  }
+  
+  .three-dots {
+    cursor: pointer;
+    padding: 5px;
+  }
+  
+  .dropdown-menu {
+    display: block;
+    position: absolute;
+    right: 0;
+    top: 20px;
+    background-color: #fff;
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    z-index: 1000;
+  }
+  
+  .menu-option {
+    padding: 10px;
+    width: 100px;
+    border: none;
+    background: none;
+    cursor: pointer;
+    text-align: left;
+  }
+  
+  .menu-option:hover {
+    background-color: #f0f0f0;
+  }
+  </style>
+  
