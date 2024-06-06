d11d9e66d2f6dd0f0c597f150f95d72132baa3c8
wliu40 <wliu0828@gmail.com>
2024-06-06 17:23:42 -0400

    nice slot example

---
+++ slot-test/slot-test/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import BadgeList from './components/BadgeList.vue';
+import userInfo from './components/UserInfo.vue';
+
+export default {
+  components: {
+    BadgeList,
+    userInfo,
+  },  
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ slot-test/slot-test/src/components/UserInfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <div>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </div>
+    <p>{{ infoText }}</p>
+  </section>
+</template>
+
+<script>
+import BaseBadge from './BaseBadge.vue';
+export default {
+  props: ['fullName', 'infoText', 'role'],
+  components: {
+    BaseBadge,
+  },
+};
+</script>
+
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
